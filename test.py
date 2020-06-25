ce587f68a7b41e5d31854a9e88199f6f2ea54116
anomaly3108 <46282160+anomaly3108@users.noreply.github.com>
2020-06-25 10:50:17 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-import random
 import unittest
 import functions
-from mock import patch
 
 
 class TestMethods(unittest.TestCase):
@@ -16,8 +14,7 @@ class TestMethods(unittest.TestCase):
         issue = {'role': ["hindi"]}
         self.assertEqual(functions.selection(mode, issue), ['agent7'])
 
-    @patch('functions.random')
-    def test_random(self, random):
+    def test_random(self):
         mode = 2 # random
         issue = {'role': ["hindi"]}
         self.assertEqual(functions.selection(mode, issue), ['agent3'] or ['agent7'])
